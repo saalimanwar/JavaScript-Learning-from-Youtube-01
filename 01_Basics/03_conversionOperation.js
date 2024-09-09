e21e286154ae8de649e0596e347f07c55d9d8685
Salim Anwar <119126143+saalimanwar@users.noreply.github.com>
2024-09-09 16:48:32 +0000

    conversion of dataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score=33
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN Not a Number
+
+let isLoggedIn=0
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1=> true; 0=> false
+// "" => false
+// "anwar" => true
