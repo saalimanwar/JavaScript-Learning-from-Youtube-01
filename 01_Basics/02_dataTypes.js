27367ecdc98c4daea3ab5f1afe02a168100adc58
Salim Anwar <119126143+saalimanwar@users.noreply.github.com>
2024-09-09 16:07:21 +0000

    DataTypes

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -1,1 +1,23 @@
-"use strict"
+"use strict" //Treat all Js code as newer version
+
+//alert(3+3) We are using node.js not web browser
+
+let name="anwar"
+let age=18
+let isLoggedIn=false;
+
+// Primitive Data Types.
+// number  => 2 to power 53
+// bigint
+// string => "anwar"
+// boolean => true/false
+// null => standalone value
+// underfined => 
+// symbol => unique     
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null) // object
+
+
